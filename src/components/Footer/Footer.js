0dd77cbbba613a5e66dee161278eb369524ecde8
VASANIYA AKASH <135131300+avasaniya@users.noreply.github.com>
2023-12-30 19:05:55 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,101 @@
+import React from "react";
+import {
+  ArrowToTop,
+  ContactHeader,
+  ContactLink,
+  Copyright,
+  FooterArrowLine,
+  FooterBigLink,
+  FooterBigLinkContainer,
+  FooterContact,
+  FooterContacts,
+  FooterLine,
+  FooterSmallTitle,
+  FooterSmallTitleWrapper,
+  FooterSocials,
+  Small,
+  StyledFooter,
+} from "../styles/Footer.styled";
+import { animateScroll as scroll } from "react-scroll";
+
+const toTop = () => {
+  scroll.scrollToTop({ delay: 0, duration: 0 });
+};
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <StyledFooter>
+      <FooterSmallTitleWrapper>
+        <FooterSmallTitle>get in touch</FooterSmallTitle>
+      </FooterSmallTitleWrapper>
+
+      <FooterBigLinkContainer>
+        <FooterBigLink
+          href="mailto:jerichobantiquete@gmail.com"
+          data-hover="Let's talk!"
+        >
+          Need a developer?
+        </FooterBigLink>
+      </FooterBigLinkContainer>
+      <FooterContacts>
+        <FooterContact>
+          <ContactHeader>Call me</ContactHeader>
+          <ContactLink href="tel:+919016664509">+919016664509</ContactLink>
+        </FooterContact>
+        <FooterContact>
+          <ContactHeader>Social</ContactHeader>{" "}
+          <FooterSocials>
+            <ContactLink
+              href="https://github.com/avasaniya"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </ContactLink>
+            <ContactLink
+              href="https://www.linkedin.com/in/vasaniya-akash/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </ContactLink>
+            <ContactLink
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </ContactLink>
+          </FooterSocials>
+        </FooterContact>
+        <FooterContact>
+          <ContactHeader>Let's grab a coffee.</ContactHeader>
+          <ContactLink href="tel:+919016664509">
+            akashvasaniya1103@gmail.com
+          </ContactLink>
+        </FooterContact>
+      </FooterContacts>
+
+      <FooterArrowLine>
+        <FooterLine />
+        <ArrowToTop title="Back to Top" onClick={toTop}></ArrowToTop>
+      </FooterArrowLine>
+
+      <Copyright>
+        <small>
+          &copy; Copyright {year},{" "}
+          <Small
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vasaniya Akash
+          </Small>
+        </small>
+      </Copyright>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
---
+++ src/components/Technologies/icons.js
@@ -0,0 +1,82 @@
+import html5 from "../../assets/technologies/html.svg";
+import css3 from "../../assets/technologies/css.svg";
+import javascript from "../../assets/technologies/javascript.svg";
+import jqvery from "../../assets/technologies/jqvery.svg";
+import reactjs from "../../assets/technologies/react.svg";
+import redux from "../../assets/technologies/redux.svg";
+import tailwindcss from "../../assets/technologies/tailwindcss.svg";
+import bootstrap from "../../assets/technologies/bootstrap.svg";
+import nodejs from "../../assets/technologies/nodejs.svg";
+import npm from "../../assets/technologies/npm.svg";
+import git from "../../assets/technologies/git.svg";
+import github from "../../assets/technologies/github.svg";
+import vscode from "../../assets/technologies/visual-studio-code.svg";
+
+export const iconsData = [
+  {
+    id: 1,
+    icon: html5,
+    name: "HTML5",
+  },
+  {
+    id: 2,
+    icon: css3,
+    name: "CSS3",
+  },
+  {
+    id: 3,
+    icon: javascript,
+    name: "JavaScript",
+  },
+  {
+    id: 4,
+    icon: jqvery,
+    name: "jqvery",
+  },
+  {
+    id: 5,
+    icon: reactjs,
+    name: "React JS",
+  },
+  {
+    id: 6,
+    icon: redux,
+    name: "Redux",
+  },
+  {
+    id: 7,
+    icon: tailwindcss,
+    name: "TailwindCSS",
+  },
+  {
+    id: 7,
+    icon: bootstrap,
+    name: "Bootstrap",
+  },
+  {
+    id: 8,
+    icon: nodejs,
+    name: "nodejs",
+  },
+  {
+    id: 9,
+    icon: npm,
+    name: "Node Package Manager",
+  },
+  {
+    id: 10,
+    icon: git,
+    name: "Git",
+  },
+  {
+    id: 11,
+    icon: github,
+    name: "GitHub",
+  },
+
+  {
+    id: 12,
+    icon: vscode,
+    name: "Visual Studio Code",
+  },
+];
